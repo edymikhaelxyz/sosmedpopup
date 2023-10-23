1fc7d495e3eaa3270f3b83ab94ef82167f4e7ab7
emns <edymikhaelxyz24@gmail.com>
2023-10-23 15:21:45 +0700

    Add files via upload

---
+++ Popup Files/settings-and-images/settings.js
@@ -0,0 +1,18 @@
+var settings = {
+    "options": {
+        "pauseTime": "5",
+        "inbetweenPauseTime": "20"
+    },
+    "colors": {
+        "iconBoxColor": "rgba(144,0,144,1)",
+        "textBoxColor": "rgba(255,255,255,1)",
+        "iconColor": "rgba(0, 0, 0, 1)",
+        "fontColor": "rgba(0,0,0,1)"
+    },
+    "fonts": {
+        "primaryFont": "ADLaM Display",
+        "fontWeight": "500",
+        "fontSize": "43",
+        "textYOffset": "0"
+    }
+};
